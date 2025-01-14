7e1184b599d7c63ff15f5f99ef7e09566aa54003
butriman <butriman@mail.ru>
2025-01-14 05:57:51 +0300

    raw-etl part finalization

--- ddl.sql
+++ ddl.sql
@@ -1,3 +1,4 @@
+-- raw layer
 CREATE TABLE raw.exchange_api_kline (
 	exchange varchar NOT NULL,
 	symbol varchar NULL,
@@ -10,4 +11,32 @@ CREATE TABLE raw.exchange_api_instrument_info (
 	exchange varchar NOT NULL,
 	insert_ts numeric NOT NULL,
 	"data" jsonb NULL
+);
+
+-- dm layer
+CREATE TABLE spot.dim_coin (
+	exchange varchar NOT NULL,
+	symbol varchar NOT NULL,
+	base_coin varchar NOT NULL,
+	quote_coin varchar NOT NULL,
+	trading_status varchar NULL,
+	insert_ts numeric NULL,
+	CONSTRAINT coin_pk PRIMARY KEY (exchange, symbol)
+);
+
+CREATE TABLE spot.sfct_exchange_rate (
+	exchange varchar NOT NULL, -- k
+	quote_coin varchar NOT NULL, -- k
+	oper_dt date NOT NULL, -- k
+	usd_amt numeric NULL,
+	insert_ts numeric NULL,
+	CONSTRAINT exchange_rate_pk PRIMARY KEY (exchange, quote_coin, oper_dt)
+);
+
+CREATE TABLE spot.tfct_coin (
+	exchange varchar NOT NULL, -- k
+	symbol varchar NOT NULL, -- k
+	oper_dt date NOT NULL, -- k
+	vol_amt numeric NULL,
+	CONSTRAINT tfct_coin_pk PRIMARY KEY (exchange, symbol, oper_dt)
 );
--- raw_etl.py
+++ raw_etl.py
@@ -2,30 +2,35 @@ from abc import ABC, abstractmethod
 from typing import Literal, TYPE_CHECKING
 import sqlalchemy as sa
 import datetime
+import calendar
 import pandas as pd
 
 if TYPE_CHECKING:
     from exchange import Bybit
 
 
-class ETLoader:
+class RawETLoader:
     start_dt: datetime.datetime
     end_dt: datetime.datetime
     #mode: Literal['incremental', 'initial']  #self.mode = mode
     db_engine: sa.Engine
+    db_schema: str = 'raw'
+
 
     def __init__(self) -> None:
         self.db_engine = sa.create_engine(
-            "postgresql+psycopg2://postgres:postgres@localhost:5432/bhft"  # не выношу креды отдельно, но по-хорошему надо
+            "postgresql+psycopg2://postgres:postgres@localhost:5432/bhft",  # не выношу креды отдельно, но по-хорошему надо
+            connect_args={'options': '-csearch_path={}'.format(self.db_schema)}
         )
         self.raw_metadata: sa.MetaData = sa.MetaData(schema='raw')
         self.raw_metadata.reflect(bind=self.db_engine)
         #self.dm_metadata: sa.MetaData = sa.MetaData(schema='dm')
         #self.dm_metadata.reflect(bind=self.db_engine)
-        print('ETLoader initialized!')
+        print('RawETLoader initialized!')
         return None
 
-    def raw_load_info(self, data: dict):
+
+    def info_insert(self, data: dict):
         info_raw_tbl = sa.Table('exchange_api_instrument_info', self.raw_metadata)
         
         with self.db_engine.connect() as conn:
@@ -35,7 +40,8 @@ class ETLoader:
             conn.commit()
         return None
     
-    def raw_load_kline(self, data: list):
+
+    def kline_insert(self, data: list):
         kline_raw_tbl = sa.Table('exchange_api_kline', self.raw_metadata)
         """
         with self.db_engine.connect() as conn:
@@ -52,3 +58,64 @@ class ETLoader:
             )
             conn.commit()
         return None
+    
+
+    def info_read(self, mode: Literal['incremental', 'initial'] = 'incremental', start_dt: datetime.datetime | None = None) -> pd.DataFrame:
+        def extract_keys(row) -> pd.DataFrame | None:
+            if row['data']:
+                json_items = row['data'].get('result', {}).get('list', {})
+                df_items = pd.json_normalize(json_items)
+                df_items['exchange'], df_items['insert_ts'] = row['exchange'], row['insert_ts']
+                return df_items[['exchange', 'insert_ts', 'symbol', 'baseCoin', 'quoteCoin', 'status']]
+            
+        if mode == 'initial':
+            with self.db_engine.connect() as conn:
+                df_info = pd.read_sql_table('exchange_api_instrument_info', conn)
+        elif mode == 'incremental':
+            with self.db_engine.connect() as conn: 
+                dt_condition = start_dt if start_dt else calendar.timegm((datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=1)).date().timetuple()) * 1000
+                df_info = pd.read_sql_query(f'select * from raw.exchange_api_instrument_info where insert_ts >= {dt_condition}', conn)
+
+        if not df_info.empty:
+            df_info_flat: pd.DataFrame = pd.concat(df_info.apply(extract_keys, axis=1).to_list(), ignore_index=True) # type: ignore
+            df_info_flat['rn'] = df_info_flat.groupby(['exchange', 'symbol', 'baseCoin', 'quoteCoin', 'status'])['insert_ts'].rank(method='first', ascending=False)
+            df_info_flat = df_info_flat[['exchange', 'symbol', 'baseCoin', 'quoteCoin', 'status', 'insert_ts']][df_info_flat['rn'] == 1].reset_index(drop=True)
+            df_info_flat.columns = ['exchange', 'symbol', 'base_coin', 'quote_coin', 'trading_status', 'insert_ts']
+            return df_info_flat
+        else:
+            print('Warning: no data found in db table!')
+            return pd.DataFrame()
+    
+
+    def kline_read(self, mode: Literal['incremental', 'initial'] = 'incremental', start_dt: datetime.datetime | None = None) -> pd.DataFrame:
+        def extract_keys(row) -> pd.DataFrame | None:
+            if row['data']:
+                json_items = row['data'].get('result', {})
+                df_items = pd.json_normalize(json_items)
+                df_items['exchange'], df_items['symbol'], df_items['insert_ts'] = row['exchange'], row['symbol'], row['insert_ts']
+                if not df_items[df_items['list'].astype(bool)].empty:
+                    return df_items.explode('list').reset_index(drop=True)
+
+        if mode == 'initial':
+            with self.db_engine.connect() as conn:
+                df_kline = pd.read_sql_table('exchange_api_kline', conn)
+        elif mode == 'incremental':
+            with self.db_engine.connect() as conn: 
+                dt_condition = start_dt if start_dt else calendar.timegm((datetime.datetime.now(tz=datetime.timezone.utc) - datetime.timedelta(days=1)).date().timetuple()) * 1000
+                df_kline = pd.read_sql_query(f'select * from raw.exchange_api_kline where insert_ts >= {dt_condition}', conn)
+
+        if not df_kline.empty:
+            df_kline_flat: pd.DataFrame = pd.concat(df_kline.apply(extract_keys, axis=1).to_list(), ignore_index=True) # type: ignore
+            df_kline_flat[['startTime', 'openPrice', 'highPrice', 'lowPrice', 'closePrice', 'volume', 'turnover']] = pd.DataFrame(df_kline_flat.list.values.tolist(), index=df_kline_flat.index)
+            df_kline_flat = df_kline_flat[['exchange', 'symbol', 'startTime', 'openPrice', 'volume', 'insert_ts']]\
+                .rename(columns={'startTime': 'report_dt', 'openPrice': 'open_price', 'volume': 'vol_amt'})\
+                .astype({'report_dt': 'Int64', 'open_price': 'Float64', 'vol_amt': 'Float64'})\
+                .drop_duplicates()
+            df_kline_flat['report_dt'] = pd.to_datetime(df_kline_flat['report_dt'], unit='ms')
+
+            df_kline_flat['rn'] = df_kline_flat.groupby(['exchange', 'symbol', 'report_dt', 'open_price', 'vol_amt'])['insert_ts'].rank(method='first', ascending=False)
+            df_kline_flat = df_kline_flat[['exchange', 'symbol', 'report_dt', 'open_price', 'vol_amt', 'insert_ts']][df_kline_flat['rn'] == 1].reset_index(drop=True)
+            return df_kline_flat
+        else:
+            print('Warning: no data found in db table!')
+            return pd.DataFrame()
