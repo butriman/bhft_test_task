7364d786cefb1c45e03762fb29d315a9f918e94d
butriman <butriman@mail.ru>
2025-01-15 00:41:10 +0300

    dm-etl finalization

--- ddl.sql
+++ ddl.sql
@@ -24,13 +24,13 @@ CREATE TABLE spot.dim_coin (
 	CONSTRAINT coin_pk PRIMARY KEY (exchange, symbol)
 );
 
-CREATE TABLE spot.sfct_exchange_rate (
+CREATE TABLE spot.tfct_exchange_rate (
 	exchange varchar NOT NULL, -- k
-	quote_coin varchar NOT NULL, -- k
+	coin varchar NOT NULL, -- k
 	oper_dt date NOT NULL, -- k
-	usd_amt numeric NULL,
+	usdt_amt numeric NULL,
 	insert_ts numeric NULL,
-	CONSTRAINT exchange_rate_pk PRIMARY KEY (exchange, quote_coin, oper_dt)
+	CONSTRAINT exchange_rate_pk PRIMARY KEY (exchange, coin, oper_dt)
 );
 
 CREATE TABLE spot.tfct_coin (
@@ -38,5 +38,21 @@ CREATE TABLE spot.tfct_coin (
 	symbol varchar NOT NULL, -- k
 	oper_dt date NOT NULL, -- k
 	vol_amt numeric NULL,
+	insert_ts numeric NULL,
 	CONSTRAINT tfct_coin_pk PRIMARY KEY (exchange, symbol, oper_dt)
 );
+
+
+-- view
+
+create materialized view if not exists spot.v_coin_volume as
+select tc.exchange, tc.symbol, tc.oper_dt, tc.vol_amt * coalesce(ter.usdt_amt, 1.0) as vol_usdt
+  from spot.tfct_coin tc 
+  left join spot.dim_coin dc 
+    on tc.exchange = dc.exchange 
+   and tc.symbol = dc.symbol 
+  left join spot.tfct_exchange_rate ter
+    on dc.quote_coin = ter.coin 
+   and dc.quote_coin <> 'USDT'
+   and tc.oper_dt = ter.oper_dt
+   and tc.exchange = ter.exchange;
--- raw_etl.py
+++ raw_etl.py
@@ -1,18 +1,18 @@
 from abc import ABC, abstractmethod
 from typing import Literal, TYPE_CHECKING
 import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import insert, Insert
+from sqlalchemy.dialects._typing import _OnConflictWhereT, _OnConflictSetT
 import datetime
 import calendar
 import pandas as pd
+from pandas.io.sql import SQLTable
 
 if TYPE_CHECKING:
     from exchange import Bybit
 
 
 class RawETLoader:
-    start_dt: datetime.datetime
-    end_dt: datetime.datetime
-    #mode: Literal['incremental', 'initial']  #self.mode = mode
     db_engine: sa.Engine
     db_schema: str = 'raw'
 
@@ -22,16 +22,14 @@ class RawETLoader:
             "postgresql+psycopg2://postgres:postgres@localhost:5432/bhft",  # не выношу креды отдельно, но по-хорошему надо
             connect_args={'options': '-csearch_path={}'.format(self.db_schema)}
         )
-        self.raw_metadata: sa.MetaData = sa.MetaData(schema='raw')
-        self.raw_metadata.reflect(bind=self.db_engine)
-        #self.dm_metadata: sa.MetaData = sa.MetaData(schema='dm')
-        #self.dm_metadata.reflect(bind=self.db_engine)
+        self.metadata: sa.MetaData = sa.MetaData(schema=self.db_schema)
+        self.metadata.reflect(bind=self.db_engine)
         print('RawETLoader initialized!')
         return None
 
 
     def info_insert(self, data: dict):
-        info_raw_tbl = sa.Table('exchange_api_instrument_info', self.raw_metadata)
+        info_raw_tbl = sa.Table('exchange_api_instrument_info', self.metadata)
         
         with self.db_engine.connect() as conn:
             conn.execute(
@@ -42,7 +40,7 @@ class RawETLoader:
     
 
     def kline_insert(self, data: list):
-        kline_raw_tbl = sa.Table('exchange_api_kline', self.raw_metadata)
+        kline_raw_tbl = sa.Table('exchange_api_kline', self.metadata)
         """
         with self.db_engine.connect() as conn:
             for row in data:
@@ -108,14 +106,128 @@ class RawETLoader:
             df_kline_flat: pd.DataFrame = pd.concat(df_kline.apply(extract_keys, axis=1).to_list(), ignore_index=True) # type: ignore
             df_kline_flat[['startTime', 'openPrice', 'highPrice', 'lowPrice', 'closePrice', 'volume', 'turnover']] = pd.DataFrame(df_kline_flat.list.values.tolist(), index=df_kline_flat.index)
             df_kline_flat = df_kline_flat[['exchange', 'symbol', 'startTime', 'openPrice', 'volume', 'insert_ts']]\
-                .rename(columns={'startTime': 'report_dt', 'openPrice': 'open_price', 'volume': 'vol_amt'})\
-                .astype({'report_dt': 'Int64', 'open_price': 'Float64', 'vol_amt': 'Float64'})\
+                .rename(columns={'startTime': 'oper_dt', 'openPrice': 'open_price', 'volume': 'vol_amt'})\
+                .astype({'oper_dt': 'Int64', 'open_price': 'Float64', 'vol_amt': 'Float64'})\
                 .drop_duplicates()
-            df_kline_flat['report_dt'] = pd.to_datetime(df_kline_flat['report_dt'], unit='ms')
+            df_kline_flat['oper_dt'] = pd.to_datetime(df_kline_flat['oper_dt'], unit='ms')
 
-            df_kline_flat['rn'] = df_kline_flat.groupby(['exchange', 'symbol', 'report_dt', 'open_price', 'vol_amt'])['insert_ts'].rank(method='first', ascending=False)
-            df_kline_flat = df_kline_flat[['exchange', 'symbol', 'report_dt', 'open_price', 'vol_amt', 'insert_ts']][df_kline_flat['rn'] == 1].reset_index(drop=True)
+            df_kline_flat['rn'] = df_kline_flat.groupby(['exchange', 'symbol', 'oper_dt', 'open_price', 'vol_amt'])['insert_ts'].rank(method='first', ascending=False)
+            df_kline_flat = df_kline_flat[['exchange', 'symbol', 'oper_dt', 'open_price', 'vol_amt', 'insert_ts']][df_kline_flat['rn'] == 1].reset_index(drop=True)
             return df_kline_flat
         else:
             print('Warning: no data found in db table!')
             return pd.DataFrame()
+        
+
+class DmETLoader:
+    db_engine: sa.Engine
+    db_schema: str = 'spot'
+
+
+    def __init__(self) -> None:
+        self.db_engine = sa.create_engine(
+            "postgresql+psycopg2://postgres:postgres@localhost:5432/bhft",  # не выношу креды отдельно, но по-хорошему надо
+            connect_args={'options': '-csearch_path={}'.format(self.db_schema)}
+        )
+        self.metadata: sa.MetaData = sa.MetaData(schema=self.db_schema)
+        self.metadata.reflect(bind=self.db_engine)
+        print('DmETLoader initialized!')
+
+
+    def get_tbl_cols(self, tbl_name: str) -> list:
+        tbl = sa.Table(tbl_name, self.metadata)
+        return [col.name for col in tbl.columns]
+
+    """
+    def __build_col_set(self, tbl_name: Literal['dim_coin', 'tfct_coin', 'tfct_exchange_rate'], insert_stmt: Insert) -> _OnConflictSetT:
+        if tbl_name == 'dim_coin':
+            return {c.key: c for c in insert_stmt.excluded}
+        elif tbl_name == 'tfct_coin':
+            return {c.key: c for c in insert_stmt.excluded if c.key not in ['open_price']}
+        elif tbl_name == 'tfct_exchange_rate':
+            return {c.key: c for c in insert_stmt.excluded if c.key not in ['vol_amt']}
+
+    
+    def __build_key_list(self, tbl_name: Literal['dim_coin', 'tfct_coin'], keys: list) -> list:
+        if tbl_name == 'dim_coin':
+            return keys
+        elif tbl_name == 'tfct_coin':
+            return [key for key in keys if key not in ['open_price']]
+    """
+    
+    def __build_where_clause(self, tbl: SQLTable, insert_stmt: Insert) -> _OnConflictWhereT:
+        if tbl.name == 'dim_coin':
+            return ((tbl.table.c.insert_ts < insert_stmt.excluded.insert_ts) & \
+                    ((tbl.table.c.base_coin != insert_stmt.excluded.base_coin) | \
+                    (tbl.table.c.quote_coin != insert_stmt.excluded.quote_coin) | \
+                    (tbl.table.c.trading_status != insert_stmt.excluded.trading_status)))
+        elif tbl.name == 'tfct_coin':
+            return ((tbl.table.c.insert_ts < insert_stmt.excluded.insert_ts) & \
+                    (tbl.table.c.vol_amt != insert_stmt.excluded.vol_amt))
+        elif tbl.name == 'tfct_exchange_rate':
+            return ((tbl.table.c.insert_ts < insert_stmt.excluded.insert_ts) & \
+                    (tbl.table.c.usdt_amt != insert_stmt.excluded.usdt_amt))
+        
+
+    def tbl_load(self, tbl_name: str, df_tbl: pd.DataFrame) -> None:
+        def upsert_on_conflict(table, conn, keys, data_iter):
+            insp = sa.inspect(conn)
+            #keys_modified = self.__build_key_list(table.name, keys)
+            #data = [dict(zip(keys_modified, row)) for row in data_iter]
+            data = [dict(zip(keys, row)) for row in data_iter]
+            insert_statement = insert(table.table).values(data)
+            upsert_statement = insert(table.table).values(data).on_conflict_do_update(
+                index_elements=insp.get_pk_constraint(table_name=table.name)["constrained_columns"],
+                set_={c.key: c for c in insert_statement.excluded}, 
+                #self.__build_col_set(table.name, insert_statement),
+                where=self.__build_where_clause(table, insert_statement)
+            )
+            result = conn.execute(upsert_statement)
+            return result.rowcount
+        
+        with self.db_engine.connect() as conn:
+            rows_affected = df_tbl.to_sql(tbl_name, conn, if_exists='append', index=False, method=upsert_on_conflict)
+        print(f'Info: upsert {rows_affected} строк(и) в таблицу {self.db_schema}.{tbl_name}')
+
+
+    """
+    def dim_coin_load(self, df_tbl: pd.DataFrame) -> None:
+        def upsert_on_conflict(table, conn, keys, data_iter):
+            insp = sa.inspect(conn)
+            data = [dict(zip(keys, row)) for row in data_iter]
+            insert_statement = insert(table.table).values(data)
+            upsert_statement = insert(table.table).values(data).on_conflict_do_update(
+                index_elements=insp.get_pk_constraint(table_name=table.name)["constrained_columns"],
+                set_={c.key: c for c in insert_statement.excluded},
+                where=((table.table.c.insert_ts < insert_statement.excluded.insert_ts) & \
+                    ((table.table.c.base_coin != insert_statement.excluded.base_coin) | \
+                        (table.table.c.quote_coin != insert_statement.excluded.quote_coin) | \
+                        (table.table.c.trading_status != insert_statement.excluded.trading_status)))
+            )
+            result = conn.execute(upsert_statement)
+            return result.rowcount
+
+        with self.db_engine.connect() as conn:
+            rows_affected = df_tbl.to_sql('dim_coin', conn, if_exists='append', index=False, method=upsert_on_conflict)
+        print(f'Info: upsert {rows_affected} строк(и) в таблицу {self.db_schema}.dim_coin')
+
+    
+    def tfct_coin_load(self, df_tbl: pd.DataFrame) -> None:
+        def upsert_on_conflict(table, conn, keys, data_iter):
+            insp = sa.inspect(conn)
+            data = [dict(zip(keys, row)) for row in data_iter]
+            insert_statement = insert(table.table).values(data)
+            upsert_statement = insert(table.table).values(data).on_conflict_do_update(
+                index_elements=insp.get_pk_constraint(table_name=table.name)["constrained_columns"],
+                set_={c.key: c for c in insert_statement.excluded},
+                where=((table.table.c.insert_ts < insert_statement.excluded.insert_ts) & \
+                    (table.table.c.vol_amt != insert_statement.excluded.vol_amt))
+            )
+            result = conn.execute(upsert_statement)
+            return result.rowcount
+
+        with self.db_engine.connect() as conn:
+            rows_affected = df_tbl.to_sql('tfct_coin', conn, if_exists='append', index=False, method=upsert_on_conflict)
+        
+        print(f'Info: upsert {rows_affected} строк(и) в таблицу {self.db_schema}.dim_coin')
+    """
